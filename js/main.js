0b85bee8ee9a584d95ea561e70366051f818ee4a
mailencantarutti <cantaruttimai@gmail.com>
2025-10-25 12:29:19 -0300

    detalles finales

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("Portfolio cargado correctamente!");
+// Lightbox
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+document.querySelectorAll('.certificate-img').forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// Cerrar al hacer clic fuera de la imagen
+lightbox.addEventListener('click', e => {
+    if(e.target !== lightboxImg) {
+        lightbox.style.display = 'none';
+    }
+});
